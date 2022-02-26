189d5414d0ffc6be6153cf2dfb25032d6d67f4f5
thebalanar <aamritbistaa@gmail.com>
2022-02-26 14:15:13 +0545

    Nepse Data extractor

--- scrapper.py
+++ scrapper.py
@@ -16,9 +16,14 @@ def wait(self):
     time.sleep(self)
 
 #KEY=input("Enter company name:  ")
-KEY="idbl"
+
+KEY=input("Enter company Name or Symbol: ")
+PAGENO=int(input("Enter total number of page: "))
+
 wait(3)
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#PATH = "C:\Users\thebalanar\Desktop\Data scrapper\chromedriver.exe"
+#driver = webdriver.Chrome(PATH)
+driver = webdriver.Chrome()
 wait(1)
 url="https://www.sharesansar.com/"
 wait(1)
@@ -58,18 +63,22 @@ wait(1)
 
 tableHeading=driver.find_element(By.XPATH,'//*[@id="myTableCPriceHistory"]/thead')
 print(tableHeading.text)
-
-while(1):
-    #table_data=driver.find_elements(By.XPATH,'//table[@id="myTableCPriceHistory"]')
+table_data_list=[]
+for i in range(0,PAGENO):
+    table_data=driver.find_elements(By.XPATH,'//table[@id="myTableCPriceHistory"]')
     tableData=driver.find_elements(By.XPATH,'//*[@id="myTableCPriceHistory"]/tbody')
-    for tableData in tableData:
-    #    print(tableData.text)
-
-        print(tableData[0])
-
-
-
-    driver.find_element(By.XPATH,'//*[@id="myTableCPriceHistory_next"]').click()
+    for tableiData in tableData:
+        print(tableiData.text)
+        c = tableiData.text
+        table_data_list.append(c)
+#         textfile = open("a_file.txt", "a")
+#         textfile.write(c + "\n")
+#         textfile.close()
+        
+    if i==PAGENO-1:
+        break
+    else:
+        driver.find_element(By.XPATH,'//*[@id="myTableCPriceHistory_next"]').click()
 
    
     #<a class="paginate_button next disabled" aria-controls="myTableCPriceHistory" data-dt-idx="7" tabindex="0" id="myTableCPriceHistory_next">Next</a>
@@ -81,3 +90,15 @@ while(1):
 print("End of file")
 
 driver.close()
+
+
+z=[]
+for i in range(0,PAGENO):
+    cdd = table_data_list[i].split("\n")
+    for item in cdd:
+        cdd_s = item.split(" ")
+        z.append(list(cdd_s))
+        
+df = pd.DataFrame(z, columns =['SN', 'Date',"Open","HIGH","lOW","LTP","%change","Qty","Turnover"])
+
+SF = df.to_csv(f"Data\{KEY}.csv")
